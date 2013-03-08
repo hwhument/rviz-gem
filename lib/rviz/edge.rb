e16473a3f055c4c55606a7a65e15c23b8366a52a
Huang Wei <huangw@pe-po.com>
2013-03-08 10:31:50 +0800

    add spec and code for edge class.

--- lib/rviz/edge.rb
+++ lib/rviz/edge.rb
@@ -7,11 +7,24 @@ module Rviz
       @from, @from_anchor, @to, @to_anchor, @attrs = from_node, from_anchor, to_node, to_anchor, attributes
     end
 
+    def node_anchor node, anchor
+      if anchor and anchor.size > 0
+        quote(node) + ":" + anchor
+      else
+        quote(node)
+      end
+    end
+
     include Helper
 
     # output to dot format
     def to_s
-      attr_str = self.attrs_to_s
+      from, to = node_anchor(@from, @from_anchor), node_anchor(@to, @to_anchor)
+      if @attrs.keys.size > 0
+        sprintf "%s -> %s [%s]", from, to, self.attrs_to_s
+      else
+        sprintf "%s -> %s", from, to
+      end
     end
   end
 end
--- spec/rviz_helper_spec.rb
+++ spec/rviz_helper_spec.rb
@@ -8,6 +8,12 @@ describe Rviz do
       m.set("label", "full stack")
       m.attrs_to_s.should eq(%q{label="full stack"})
     end
+
+    it "return initial attributes in format" do
+      m = Rviz::Edge.new "from", "", "to", "", {arrowhead: "onormal"}
+      m.set("label", "full stack")
+      m.attrs_to_s.should eq(%q{arrowhead=onormal, label="full stack"})
+    end
   end
 end
 
