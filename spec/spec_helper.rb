f19ff12a6e80084cba7783df9dbe8bb8512f23e9
Huang Wei <huangw@pe-po.com>
2013-03-08 16:03:32 +0800

    hotfix: remove useless capture function in rspec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,15 +5,3 @@ RSpec.configure do |config|
   config.color_enabled = true
   config.formatter     = 'documentation'
 end
-
-def capture_stdout(&block)
-  original_stdout = $stdout
-  $stdout = fake = StringIO.new
-  begin
-    yield
-  ensure
-    $stdout = original_stdout
-  end
-  fake.string
-end
-
